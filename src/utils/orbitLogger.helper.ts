a1f30f48f86c94f62880577ab147bb13c13acf9d
Orbit Turner <orbitturner@gmail.com>
2023-11-23 12:03:09 +0000

    feat(mantis-cli): :sparkles: Added Default Options for Logger
    Added Logs Folder, Ignored .log files and added default options to OrbitLogger

--- src/utils/orbitLogger.helper.ts
+++ src/utils/orbitLogger.helper.ts
@@ -3,10 +3,29 @@ import Logger, { LoggerOptions } from '@ptkdev/logger';
 export class OrbitLogger {
     private logger: Logger;
     private context: string;
+    private defaultOptions: LoggerOptions = {
+        language: "en",
+        colors: true,
+        debug: true,
+        info: true,
+        warning: true,
+        error: true,
+        sponsor: true,
+        write: true,
+        type: "log",
+        rotate: {
+            size: "10M",
+            encoding: "utf8",
+        },
+        path: {
+            debug_log: "../logs/debug.log",
+            error_log: "../logs/errors.log",
+        },
+    };
 
     constructor(context: string, options?: LoggerOptions) {
         this.context = context;
-        this.logger = new Logger(options);
+        this.logger = new Logger(options || this.defaultOptions);
     }
 
     debug(message: string, dump?: any, tag?: string) {
