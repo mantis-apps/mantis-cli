17699e7ccc6dd861f16930082b6bb3fdcfee32bf
Orbit Turner <orbitturner@gmail.com>
2023-11-23 16:36:41 +0000

    docs(mantis-cli): :memo: Added Color Palette Link

--- src/utils/welcome.helper.ts
+++ src/utils/welcome.helper.ts
@@ -3,7 +3,8 @@ import { clearConsole } from './utilities.helper';
 import { printWithMantisGradient } from './prettyPrint.helper';
 
 /**
- * Prints a welcome message using figlet and chalk/chalk-animation.
+ * Prints a welcome message using figlet and gradient.
+ * @colorPalette https://coolors.co/palette/d9ed92-b5e48c-99d98c-76c893-52b69a-34a0a4-168aad-1a759f-1e6091-184e77
  */
 export const welcome = async () => {
   clearConsole();
