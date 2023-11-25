44b39435a8bdcfa87d26c21b01d2ddc611c51fa4
Orbit Turner <orbitturner@gmail.com>
2023-11-25 16:48:07 +0000

    Added QuickTodo to command file

--- src/commands/start.command.ts
+++ src/commands/start.command.ts
@@ -4,6 +4,10 @@ import { ActionFile } from 'src/actions/actionTypes';
 
 const currentCommandName = path.basename(__filename).split('.').at(0);
 
+/**
+* TODO: Create an Action Loader function that will load action when given {this} 
+* See if we cant Create a Get Command Name function for current file name.
+*/
 export default new Command(currentCommandName)
     .description('Description of someCommand')
     .action((require(`../actions/${currentCommandName}.action`) as ActionFile)[`${currentCommandName}Action`]);
